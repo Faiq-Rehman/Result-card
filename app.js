a3bc0ad6daef5422c267158c1f846cac8262c370
Faiq-Rehman <faiqrahman00021@gmail.com>
2025-03-27 22:51:00 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
         function calculateResult() {
-            let math = parseInt(document.getElementById("math").value) || 0;
-            let urdu = parseInt(document.getElementById("urdu").value) || 0;
-            let english = parseInt(document.getElementById("english").value) || 0;
-            let comp = parseInt(document.getElementById("comp").value) || 0;
-            let physics = parseInt(document.getElementById("physics").value) || 0;
+            let math = Number(document.getElementById("math").value) || 0;
+            let urdu = Number(document.getElementById("urdu").value) || 0;
+            let english = Number(document.getElementById("english").value) || 0;
+            let comp = Number(document.getElementById("comp").value) || 0;
+            let physics = Number(document.getElementById("physics").value) || 0;
             
             let total = math + urdu + english + comp + physics;
             let percentage = (total / 500) * 100;
